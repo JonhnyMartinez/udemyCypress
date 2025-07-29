53ca042d2c7218efd071a4c57ead92749632a4c0
jmartinez <jmartinez@excelsis.com.py>
2025-07-29 16:51:18 -0300

    ultimos cambios

---
+++ cypress/e2e/custom_comand.cy.js
@@ -0,0 +1,69 @@
+/// <reference types="cypress"/>
+require("cypress-plugin-tab");
+import "cypress-xpath";
+const XLSX = require("xlsx");
+//import "cypress-file-upload";
+import { faker } from "@faker-js/faker";
+
+let tp = 1500;
+let ttiempo_espera = 5000;
+let nombreGlobal = "";
+let sessionCookies = [];
+function scroll(x, y, t) {
+  cy.window().then((win) => {
+    win.scrollBy(x, y);
+    cy.wait(t);
+  });
+}
+Cypress.on("uncaught:exception", (err, runnable) => {
+  // Evitar que la excepción detenga la ejecución
+  return false;
+});
+
+describe("Funciones Propias personalizadas", () => {
+  beforeEach(() => {
+    cy.VentanaXY(1900, 900);
+  });
+
+  it("Login", () => {
+    cy.Ventana_Inicio("https://www.saucedemo.com/", "Swag", tp);
+
+    cy.Login_Basico(
+      '[data-test="username"]',
+      "standard_user",
+      '[data-test="password"]',
+      "secret_sauce",
+      "#login-button",
+      tp
+    );
+  });
+  it("Login dos por objeto", () => {
+    cy.Ventana_Inicio("https://www.saucedemo.com/", "Swag", tp);
+    cy.Login_Basico2(
+      {
+        usernameSelector: '[data-test="username"]',
+        username: "standard_user",
+        passwordSelector: '[data-test="password"]',
+        password: "secret_sauce",
+        buttonSelector: "#login-button",
+      },
+      { s1: '[data-test="title"]', texto: "Products" },
+      3000
+    );
+  });
+  it.only("funciones uno", () => {
+    cy.Ventana_Inicio(
+      "https://www.salesforce.com/mx/form/signup/freetrial-sales-ee/?d=70130000000EqoPAAS",
+      "Sales",
+      tp
+    );
+    //cy.Click_Css("");
+    cy.Texto_Xpath('//*[@id="UserFirstName-kxKb"]', "Rodrigo", tp);
+    cy.Texto_Name("userFirstName", "Juan", tp);
+    cy.Texto_Etiqueta("Nombre", "Pedro", tp);
+    cy.Texto_Etiqueta("Apellidos", "Perez", tp);
+    cy.Texto_Etiqueta("Posición", "Sistemas", tp);
+    cy.Validar_Error_Xpath("", "Introduzca una correo electrónico", tp);
+    cy.Validar_Error_Xpath("", "Introduzca un número de teléfono", tp);
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,6 +23,77 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { faker } from "@faker-js/faker";
+import cypress from "cypress";
+Cypress.Commands.add(
+  "Login_Basico",
+  (s1, username, s2, password, button, tiempo = 1000) => {
+    cy.get(s1).type(username);
+    cy.wait(tiempo);
+    cy.get(s2).type(password);
+    cy.wait(tiempo);
+    cy.get(button).click({ time: 5000 });
+    cy.Validar_Texto('[data-test="title"]', "Products", 2000);
+    cy.wait(tiempo);
+  }
+);
+
+Cypress.Commands.add("VentanaXY", (x, y, tiempo = 1000) => {
+  cy.viewport(x, y);
+  cy.wait(tiempo);
+  cy.log("Tamaño de la venta " + x + " " + y);
+});
+
+Cypress.Commands.add("Ventana_Inicio", (url, title, tiempo = 1000) => {
+  cy.visit(url);
+  cy.title().should("contain", title);
+  cy.wait(tiempo);
+  cy.log("Url" + url + "Titulo " + title);
+});
+
+Cypress.Commands.add("Login_Basico2", (options, options2, tiempo = 1000) => {
+  const {
+    usernameSelector,
+    username,
+    passwordSelector,
+    password,
+    buttonSelector,
+  } = options;
+  const { s1, texto } = options2;
+  cy.get(usernameSelector).type(username);
+  cy.wait(tiempo);
+  cy.get(passwordSelector).type(password);
+  cy.wait(tiempo);
+  cy.get(buttonSelector).click({ timeout: 5000 });
+  cy.wait(tiempo);
+  cy.Validar_Texto(s1, texto);
+  cy.log("se llama a funcion validar texto");
+
+  cy.wait(tiempo);
+});
+Cypress.Commands.add("Validar_Texto", (s1, texto, tiempo = 100) => {
+  cy.get(s1).should("have.text", texto);
+  cy.wait(tiempo);
+  cy.log("Texto a validar: " + texto);
+});
+
+/////-----///////
+Cypress.Commands.add("Click_Xpath", (selector, tiempo = 1000) => {
+  cy.xpath(selector).should("be.visible").click({ time: 5000 });
+  cy.wait(tiempo);
+  cy.log("Click en el elemento: " + selector);
+});
+Cypress.Commands.add("Click_Css", (selector, tiempo = 1000) => {
+  cy.get(selector).should("be.visible").click({ time: 5000 });
+  cy.wait(tiempo);
+  cy.log("Click en el elemento: " + selector);
+});
+Cypress.Commands.add("Texto_Xpath", (selector, texto, tiempo = 1000) => {
+  cy.xpath(selector).should("be.visible").first().type(texto);
+  cy.wait(tiempo);
+  cy.log("texto: " + texto)
+});
+///caso aparte del comandos//
 Cypress.Commands.add("login", (username, password) => {
   cy.get('[data-test="username"]').type(username);
   cy.get('[data-test="password"]').type(password);
