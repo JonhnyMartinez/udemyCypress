375e79f525569fad00e3b90aec8b1664fc946802
jmartinez <jmartinez@excelsis.com.py>
2025-08-11 16:36:33 -0300

    ultimos cambios

--- cypress/e2e/custom_comand.cy.js
+++ cypress/e2e/custom_comand.cy.js
@@ -57,7 +57,7 @@ describe("Funciones Propias personalizadas", () => {
       3000
     );
   });
-  it.only("funciones uno", () => {
+  it("funciones uno", () => {
     //cy.Click_Css("");
     //cy.Texto_Xpath('//*[@id="UserFirstName-kxKb"]', "Rodrigo", tp);
     cy.Ventana_Inicio(
@@ -65,7 +65,27 @@ describe("Funciones Propias personalizadas", () => {
       "Sales",
       tp
     );
-    cy.textoSelector(,"Rodrigo","",tp)
+    cy.Validar_Error_Xpath(
+      "//div[contains(@class, 'field')]//input[@name='UserFirstName']",
+      "Introduzca su nombre",
+      tp
+    );
+    cy.addClaseId(
+      "//div[contains(@class, 'field')]//input[@name='UserFirstName']",
+      "className",
+      "idName",
+      20000
+    );
+    cy.textoSelector(".className", "Rodrigo", "css", tp);
+    cy.textoSelector("#idName", "Pedro", "css", tp);
+    cy.textoSelector(
+      "//div[contains(@class, 'field')]//input[@name='UserFirstName']",
+      "Carlos",
+      "xpath",
+      tp
+    );
+    cy.textoSelector("UserFirstName", "Miguel", "name", tp);
+    cy.textoSelector("Apellidos", "Erika", "label", tp);
     //cy.Texto_Name("UserFirstName", "Juan", tp);
     //cy.Texto_Etiqueta("Nombre", "Pedro", tp);
     //cy.Texto_Css(".firstName", "Juan", tp);
@@ -74,5 +94,47 @@ describe("Funciones Propias personalizadas", () => {
     cy.Texto_Etiqueta("Posición", "Sistemas", tp);
     cy.Validar_Error_Xpath("", "Introduzca una correo electrónico", tp);
     cy.Validar_Error_Xpath("", "Introduzca un número de teléfono", tp);*/
+  });
+  it("Funciones dos", () => {
+    cy.Ventana_Inicio(
+      "https://validaciones.rodrigovillanueva.com.mx/Calculador_ok.html",
+      "Formulario",
+      tp
+    );
+    cy.Texto_Etiqueta("Número 1", "10", tp);
+    cy.Texto_Etiqueta("Número 2", "20", tp);
+    cy.Click_Texto("Sumar", tp);
+    cy.Click_Texto("Restar", tp);
+    // cy.Texto_Etiqueta_Habilitar("Resultado", "400", tp);
+    cy.Copiar_Pegar_Css("#resultado", "#resultado2", tp);
+    //cy.Texto_Xpath_Habilitar('//*[@id="resultado"]', "100", tp);
+    //cy.Texto_Css_Habilitar("#resultado", "50", 3000);
+  });
+  it.only("Campos Select", () => {
+    tp = 1200;
+    cy.Ventana_Inicio(
+      "https://www.salesforce.com/mx/form/signup/freetrial-sales-ee/?d=70130000000EqoPAAS",
+      "Sales",
+      tp
+    );
+    // cy.Combo_Xpath(
+    //   "/html/body/div[4]/div/div/div[6]/div[1]/div[2]/div/div/div[2]/div/div[1]/div/form/div[9]/div/select",
+    //   "Italiano",
+    //   tp
+    // );
+    // cy.Combo_Xpath("", tp);
+    // cy.Combo_Name("CompanyLanguage", "Tai", tp);
+    // cy.Combo_Etiqueta("Idioma", "Italiano", tp);
+
+    cy.selectCombo("xpath", '//*[@id="CompanyLanguage-rwKS"]', "Tai", tp);
+    cy.selectCombo("name", "CompanyLanguage", "Inglés", tp);
+    cy.selectCombo("label", "Idioma", "Alemán", tp);
+
+    cy.selectCombo("label", "Empleados", "1 - 50 empleados", tp);
+
+    cy.addClaseId("", "classEmp", "idEmp", tp);
+
+    cy.selectCombo("css", ".classEmp", "51 - 300 empleados", tp);
+    cy.selectCombo("id", "#idEmp", "1,001 - 2,000 empleados", tp);
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,7 +25,9 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 //import { faker } from "@faker-js/faker";
 //import cypress from "cypress";
-import cypress from "cypress";
+//import cypress from "cypress";
+require("cypress-plugin-tab");
+import "cypress-xpath";
 import "cypress-real-events/support";
 Cypress.Commands.add(
   "Login_Basico",
@@ -90,11 +92,20 @@ Cypress.Commands.add("Click_Css", (selector, tiempo = 1000) => {
   cy.wait(tiempo);
   cy.log("Click en el elemento: " + selector);
 });
+
+/** */
+Cypress.Commands.add("Click_Texto", (texto, tiempo = 1000) => {
+  cy.contains(texto).should("be.visible").click({ time: 5000 });
+  cy.wait(tiempo);
+  cy.log("Click en el elemento por Css : " + texto);
+});
+/** */
 Cypress.Commands.add("Texto_Xpath", (selector, texto, tiempo = 1000) => {
   cy.xpath(selector).should("be.visible").first().type(texto);
   cy.wait(tiempo);
   cy.log("texto: " + texto);
 });
+/** */
 Cypress.Commands.add("Texto_Name", (selector, texto, tiempo = 1000) => {
   cy.get(`[name="${selector}"]`)
     .scrollIntoView()
@@ -104,9 +115,9 @@ Cypress.Commands.add("Texto_Name", (selector, texto, tiempo = 1000) => {
   cy.wait(tiempo);
   cy.log("Texto: " + selector + " " + texto);
 });
-
-Cypress.Commands.add("Texto_Etiqueta", (label, texto, tiempo = 1000) => {
-  cy.contains("label", label)
+/** */
+Cypress.Commands.add("Texto_Etiqueta", (Label, texto, tiempo = 1000) => {
+  cy.contains("label", Label)
     .invoke("attr", "for")
     .then((fieldId) => {
       cy.get(`#${fieldId}`)
@@ -118,16 +129,41 @@ Cypress.Commands.add("Texto_Etiqueta", (label, texto, tiempo = 1000) => {
         .log("Texto: " + texto);
     });
 });
+/** */
 Cypress.Commands.add("Texto_Css", (selector, texto, tiempo = 1000) => {
   cy.get(selector)
     .scrollIntoView()
     .should("be.visible")
     .first()
-    //.clear()
-    .type(texto);
+    .click()
+    .then(($el) => {
+      const existingText = $el.text();
+      if (existingText) {
+        cy.wrap($el).clear();
+      }
+      if ($el.is("input,textarea")) {
+        cy.wrap($el).val(texto);
+      } else {
+        $el.val(texto);
+      }
+    });
   cy.wait(tiempo);
   cy.log("Texto: " + texto);
 });
+//add clase y id
+Cypress.Commands.add(
+  "addClaseId",
+  (selector, clase = "", id = "", tiempo = 1000) => {
+    cy.xpath(selector)
+      .should("be.visible")
+      .then(($el) => {
+        $el.attr("class", clase);
+        $el.attr("id", id);
+      });
+    cy.wait(tiempo);
+  }
+);
+/** */
 Cypress.Commands.add(
   "textoSelector",
   (selector, texto, selectorType = "css", tiempo = 1000) => {
@@ -136,13 +172,14 @@ Cypress.Commands.add(
         cy.xpath(selector)
           .should("be.visible")
           .first()
+          .click()
           .then(($el) => {
             const existingText = $el.text();
             if (existingText) {
-              $el.clear();
+              cy.wrap($el).clear();
             }
             if ($el.is("input,textarea")) {
-              $el.val(texto);
+              cy.wrap($el).val(texto);
             } else {
               $el.val(texto);
             }
@@ -154,13 +191,15 @@ Cypress.Commands.add(
         cy.get(selector)
           .scrollIntoView()
           .should("be.visible")
+          .first()
+          .click()
           .then(($el) => {
             const existingText = $el.text();
             if (existingText) {
-              $el.clear();
+              cy.wrap($el).clear();
             }
             if ($el.is("input,textarea")) {
-              $el.val(texto);
+              cy.wrap($el).val(texto);
             } else {
               $el.val(texto);
             }
@@ -172,13 +211,15 @@ Cypress.Commands.add(
         cy.get(`[name="${selector}"]`)
           .scrollIntoView()
           .should("be.visible")
+          .first()
+          .click()
           .then(($el) => {
             const existingText = $el.text();
             if (existingText) {
-              $el.clear();
+              cy.wrap($el).clear();
             }
             if ($el.is("input,textarea")) {
-              $el.val(texto);
+              cy.wrap($el).val(texto);
             } else {
               $el.val(texto);
             }
@@ -195,10 +236,10 @@ Cypress.Commands.add(
               .should("be.visible")
               .then(($el) => {
                 if (existingText) {
-                  $el.clear();
+                  cy.wrap($el).clear();
                 }
                 if ($el.is("input,textarea")) {
-                  $el.val(texto);
+                  cy.wrap($el).val(texto);
                 } else {
                   $el.val(texto);
                 }
@@ -214,10 +255,10 @@ Cypress.Commands.add(
           .then(($el) => {
             const existingText = $el.text();
             if (existingText) {
-              $el.clear();
+              cy.wrap($el).clear();
             }
             if ($el.is("input,textarea")) {
-              $el.val(texto);
+              cy.wrap($el).val(texto);
             } else {
               $el.val(texto);
             }
@@ -230,6 +271,153 @@ Cypress.Commands.add(
     }
   }
 );
+
+/**
+ * Validar Error
+ */
+Cypress.Commands.add(
+  "Validar_Error_Xpath",
+  (selector, mensaje, tiempo = 1000) => {
+    cy.xpath(selector, { timeout: tiempo })
+      .should("be.visible", { timeout: tiempo })
+      .click()
+      .tab()
+      .wait(tiempo)
+      .then(() => {
+        cy.get("span.error-msg").should("be.visible").and("contain", mensaje);
+      });
+    cy.log("se encontro el error: " + mensaje);
+  }
+);
+/** */
+Cypress.Commands.add(
+  "Texto_Xpath_Habilitar",
+  (selector, texto, tiempo = 1000) => {
+    cy.xpath(selector)
+      .scrollIntoView()
+      .should("be.visible")
+      .then(($el) => {
+        // Verificar si está deshabilitado
+        if ($el.prop("disabled")) {
+          // Habilitar el campo si está deshabilitado
+          $el.prop("disabled", false);
+        }
+        // Volver a envolver el elemento para usar comandos Cypress
+        cy.wrap($el).clear().type(texto);
+      });
+
+    cy.wait(tiempo);
+    cy.log("Texto: " + texto);
+  }
+);
+
+/** */
+Cypress.Commands.add(
+  "Texto_Css_Habilitar",
+  (selector, texto, tiempo = 1000) => {
+    cy.get(selector)
+      .scrollIntoView()
+      .should("be.visible")
+      .then(($el) => {
+        //verificar si el campo esta deshabilitado
+        if ($el.prop("disabled")) {
+          ///remover la propiedad disabled
+          cy.wrap($el)
+            .invoke("prop", "disabled", false)
+            .should("not.be.disabled");
+        }
+      })
+      .clear()
+      .type(texto);
+    cy.wait(tiempo);
+    cy.log("Texto: " + texto);
+  }
+);
+
+Cypress.Commands.add(
+  "Texto_Etiqueta_Habilitar",
+  (Label, texto, tiempo = 1000) => {
+    cy.contains("label", Label)
+      .invoke("attr", "for")
+      .then(($fielId) => {
+        cy.get(`#${fieldId}`)
+          .first()
+          .should("be.visible")
+          .then(($el) => {
+            //verificar si el campo esta deshabilitado
+            if ($el.prop("disabled")) {
+              ///remover la propiedad disabled
+              cy.wrap($el)
+                .invoke("prop", "disabled", false)
+                .should("not.be.disabled");
+            }
+          })
+          .clear()
+          .type(texto);
+        cy.wait(tiempo);
+        cy.log("Texto: " + texto);
+      });
+  }
+);
+/** */
+Cypress.Commands.add(
+  "Copiar_Pegar_Css",
+  (campoOrigen, campoDestino, tiempo = 1000) => {
+    //copiar el valor del resultado
+    cy.get(campoOrigen).then(($origen) => {
+      const valorResultado = $origen.val();
+      cy.log("Valor Copiado: " + valorResultado);
+      //pegar el valor del resultado en el campo de destino
+      cy.get(campoDestino).clear().type(valorResultado).wait(tiempo);
+      cy.log("Valor Copiado: " + campoDestino + valorResultado);
+    });
+  }
+);
+/** */
+Cypress.Commands.add(
+  "Copiar_Pegar_Xpath",
+  (campoOrigen, campoDestino, tiempo = 1000) => {
+    //copiar el valor del resultado
+    cy.xpath(campoOrigen).then(($origen) => {
+      const valorResultado = $origen.val();
+      cy.log("Valor Copiado: " + valorResultado);
+      //pegar el valor del resultado en el campo de destino
+      cy.xpath(campoDestino).clear().type(valorResultado).wait(tiempo);
+    });
+  }
+);
+
+Cypress.Commands.add("Combo_Xpath", (selector, texto, tiempo = 1000) => {
+  cy.xpath(selector).scrollIntoView().should("be.visible").select(texto);
+  cy.wait(tiempo);
+  cy.log("Seleccion: " + texto);
+});
+Cypress.Commands.add("Combo_Css", (selector, texto, tiempo = 1000) => {
+  cy.get(selector).scrollIntoView().should("be.visible").select(texto);
+  cy.wait(tiempo);
+  cy.log("Seleccion: " + texto);
+});
+Cypress.Commands.add("Combo_Name", (selector, texto, tiempo = 1000) => {
+  cy.get(`[name="${selector}"]`)
+    .scrollIntoView()
+    .should("be.visible")
+    .select(texto);
+  cy.wait(tiempo);
+  cy.log("Seleccion: " + texto);
+});
+Cypress.Commands.add("Combo_Etiqueta", (Label, texto, tiempo = 1000) => {
+  cy.contains("label", Label)
+    .invoke("attr", "for")
+    .sthen((fieldId) => {
+      cy.get(`#${fieldId}`)
+        .first()
+        .scrollIntoView()
+        .should("be.visible")
+        .select(texto);
+      cy.wait(tiempo);
+      cy.log("Seleccion: " + texto);
+    });
+});
 ///caso aparte del comandos//
 Cypress.Commands.add("login", (username, password) => {
   cy.get('[data-test="username"]').type(username);
